d8baabf613a4f4b61fec4e8ef7e77e478c771ab7
Rose <rosewang2008@gmail.com>
2017-07-31 19:30:27 +0300

    [edited] notes under trees

---
+++ im-loving-trees.py
@@ -0,0 +1,25 @@
+# Treeeeeeees! A special, dear type of graph
+# Duh duh duh duh duuuuuh, I'm lovin' dat graph theory
+
+# root node: 0+ child nodes
+# child: 0+ child nodes
+# no cycles
+# leaf: no children
+
+# Make sure to clarify tree/graph questions! 
+
+# TREES
+	# BINARY TREES: up to two children per parent node
+		# BINARY SEARCH TREES--ordering property: all left descendants <= node < all right descendants
+			# point of clarification: duplicate values? which side of node?
+		# COMPLETE BINARY TREES: fully filled, filled left to right
+		# FULL BINARY TREES: nodes have either 0 or 2 children
+		# PERFECT BINARY TREES: FULL and COMPLETE with 2^k - 1 nodes, k = # levels
+			# leaves: same level--max # nodes
+
+	# BALANCED: insertion and find: O(log n)
+		# PERFECT BINARY TREES
+		# RED-BLACK TREES
+		# AVL TREES
+
+	# UNBALANCED
