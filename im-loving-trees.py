aa54ebaf6fd9d113da25aae85f9fb8f456ab7eaf
Rose <rosewang2008@gmail.com>
2017-08-01 23:21:00 +0300

    [edited/done?] tree, still need deleate, workin

--- im-loving-trees.py
+++ im-loving-trees.py
@@ -27,32 +27,54 @@
 ### consider redoing data structures in Java (Java's extensive 'built-in' data structures--useful)
 
 #			10[0]
-#    8 [1]  			20 [2]
+
+#    	8 [1]  				20 [2]
+
 # 4 [3]    9 [4]        15 [5]     30 [6]
 
 # binary search tree
 class Node:
 	def __init__(self, data):
 		self.data = data
-		self.children = []
-
-	def add_child(self, child):
-		self.children.append(child)
-
+		self.left_child = None
+		self.right_child = None
+		self.parent = None
 	def hasLeftChild(self):
-
+		return self.left_child != None
 	def hasRightChild(self):
-
+		return self.right_child != None
 	def isChildOf(self):
-
+		return self.parent
 	def isLeaf(self): 
-
+		return self.hasLeftChild() == False and self.hasRightChild() == True
 	def hasChild(self):
-
+		return self.left_child != None or self.right_child != None
 	def hasBothChildren(self): 
+		return self.left_child != None and self.right_child != None
 
-	def delete_node(self, node):
-		# check for existence of node
-	def find_mind(self):
+def insert(root, node):
+	# is there someway to reduce # if conditions
+	if root == None:
+		root = node
+	else: 
+		if node.data > root.data:
+			if root.right_child == None:
+				root.right_child = node
+				node.parent = root
+			else:
+				insert(root.right_child, node)
+		else: # node.data <= root.data
+			if root.left_child == None:
+				root.left_child = node
+				node.parent = root
+			else:
+				insert(root.left_child, node)
 
-	def find_max(self): 
+# def find_min(root):
+# 	while root.left_child != None:
+# def find_max(self): 
+# 
+# def delete_node(self, node):
+
+	# check for existence of node
+
