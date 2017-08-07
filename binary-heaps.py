ed1f355e7dbb8cd5cabc139a9aecbb7773e3613c
Rose <rosewang2008@gmail.com>
2017-08-07 19:33:08 +0300

    [moved] files

--- binary-heaps.py
+++
@@ -1,9 +0,0 @@
-# Binary heaps (min-heaps, max-heaps)
-# complete binary treee
-
-# MIN-HEAP:
-	# each node smaller than it children
-	# root = min element
-	# key operations:
-		# insert
-		# extract min
