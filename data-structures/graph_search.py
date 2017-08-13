70d4a1f88f113a287db63092cdaff14a0b6df0cf
Rose <rosewang2008@gmail.com>
2017-08-13 14:47:05 +0300

    [edited] graph DFS

--- data-structures/graph_search.py
+++ data-structures/graph_search.py
@@ -1,21 +1,63 @@
+class Node:
+	''' Using dictionaries for implementation
+	'''
+	def __init__(self, ID,  weight):
+		self.ID = ID
+		self.weight = weight
+		self.connections = {}
+		self.visited = False
 
+	def connect(self, other):
+		self.connections[other.ID] = [other.weight]
+		return other.ID
+
+	def getConnections(self):
+		return self.connections.keys()
+
+class Graph(Node):
+	def __init__(self):
+		self.nodes = {}
+		self.size = len(self.nodes)
+
+	def add(self, Node):
+		''' Node is already initialized. 
+		add() adds the node into the graph, 
+		along with the node's connecting vertices
+		'''
+		for conn in Node.connections.keys():
+			if Node.ID in self.nodes.keys():
+				self.nodes[Node.ID].append(conn)
+			else: 
+				self.nodes[Node.ID] = [conn]
+
+	def insert(self, ID, weight):
+		vertex = Node.__init__(self, ID, weight)
+		self.add(vertex)
 # graph search
 # DEPTH	FIRST SEARCH (DFS): exploring each branch, going DEEP!
 	# visiting every node
 	# e.g. pre-order, other tree traversals
 	# should check if node has already been visited
-def search(Node):
-	# initialize some kind of list to keep track of paths 
-	# check if exists
-	# then:
-	if Node.visited == True:
-		return
-	else: 
-		Node.visited = True
-		for neighbor in Node.connections.keys():
-			search(neighbor)
-		return Node.ID
-
+	def search(self, Node):
+		# initialize some kind of list to keep track of paths 
+		# check if exists
+		# then:
+		if Node.visited == True:
+			return
+		else: 
+			Node.visited = True
+			for neighbor in Node.connections.keys():
+				search(neighbor)
+			return Node.ID
+a = Node('a', 5)
+b = Node('b', 4)
+c = Node('c', 4)
+a.connect(b)
+a.connect(c)
+# print(a.getConnections())
+# print(a.connections)
+G = Graph()
+G.add(a)
 print(search(a))
 
 # BREADTH FIRST SEARCH (BFS): exploring each neighbor, going WIDE! 
